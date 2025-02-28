52dcabb257a340f6282c75986a7f8c847b246b95
joy <joyshin68@gmail.com>
2025-02-28 16:18:19 +0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Objectdemo
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btndiplaytest = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnGaramond = New System.Windows.Forms.Button()
+        Me.btnMagneto = New System.Windows.Forms.Button()
+        Me.btnTahoma = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.btnLoadpicture = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btndiplaytest
+        '
+        Me.btndiplaytest.Location = New System.Drawing.Point(31, 40)
+        Me.btndiplaytest.Name = "btndiplaytest"
+        Me.btndiplaytest.Size = New System.Drawing.Size(174, 79)
+        Me.btndiplaytest.TabIndex = 0
+        Me.btndiplaytest.Text = "Display Test"
+        Me.btndiplaytest.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btndiplaytest.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(55, 71)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Sample Test"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(325, 40)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(366, 79)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Font"
+        Me.Button2.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'btnGaramond
+        '
+        Me.btnGaramond.Location = New System.Drawing.Point(366, 74)
+        Me.btnGaramond.Name = "btnGaramond"
+        Me.btnGaramond.Size = New System.Drawing.Size(87, 29)
+        Me.btnGaramond.TabIndex = 3
+        Me.btnGaramond.Text = ". Garamond"
+        Me.btnGaramond.UseVisualStyleBackColor = True
+        '
+        'btnMagneto
+        '
+        Me.btnMagneto.Location = New System.Drawing.Point(471, 77)
+        Me.btnMagneto.Name = "btnMagneto"
+        Me.btnMagneto.Size = New System.Drawing.Size(75, 23)
+        Me.btnMagneto.TabIndex = 4
+        Me.btnMagneto.Text = "Magneto"
+        Me.btnMagneto.UseVisualStyleBackColor = True
+        '
+        'btnTahoma
+        '
+        Me.btnTahoma.Location = New System.Drawing.Point(581, 71)
+        Me.btnTahoma.Name = "btnTahoma"
+        Me.btnTahoma.Size = New System.Drawing.Size(75, 23)
+        Me.btnTahoma.TabIndex = 5
+        Me.btnTahoma.Text = "Tahoma"
+        Me.btnTahoma.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(441, 161)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(143, 186)
+        Me.Button3.TabIndex = 6
+        Me.Button3.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(465, 178)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "font style"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(465, 224)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 8
+        Me.Button6.Text = "italic"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(471, 263)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(69, 23)
+        Me.Button7.TabIndex = 9
+        Me.Button7.Text = "bold"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(471, 304)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 26)
+        Me.Button8.TabIndex = 10
+        Me.Button8.Text = "italic bold"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(628, 161)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(131, 186)
+        Me.Button9.TabIndex = 11
+        Me.Button9.Text = "font color"
+        Me.Button9.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.SystemColors.Control
+        Me.Button10.ForeColor = System.Drawing.Color.Green
+        Me.Button10.Location = New System.Drawing.Point(645, 193)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(75, 23)
+        Me.Button10.TabIndex = 12
+        Me.Button10.Text = "Green"
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Button11
+        '
+        Me.Button11.ForeColor = System.Drawing.Color.Blue
+        Me.Button11.Location = New System.Drawing.Point(645, 234)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(75, 23)
+        Me.Button11.TabIndex = 13
+        Me.Button11.Text = "blue"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.ForeColor = System.Drawing.Color.Red
+        Me.Button12.Location = New System.Drawing.Point(645, 281)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(75, 23)
+        Me.Button12.TabIndex = 14
+        Me.Button12.Text = "red"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'btnLoadpicture
+        '
+        Me.btnLoadpicture.Location = New System.Drawing.Point(66, 390)
+        Me.btnLoadpicture.Name = "btnLoadpicture"
+        Me.btnLoadpicture.Size = New System.Drawing.Size(75, 23)
+        Me.btnLoadpicture.TabIndex = 15
+        Me.btnLoadpicture.Text = "Load picture"
+        Me.btnLoadpicture.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(583, 411)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 16
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Objectdemo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnLoadpicture)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btnTahoma)
+        Me.Controls.Add(Me.btnMagneto)
+        Me.Controls.Add(Me.btnGaramond)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btndiplaytest)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "Objectdemo"
+        Me.Text = "Obect Demontrations"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btndiplaytest As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents btnGaramond As Button
+    Friend WithEvents btnMagneto As Button
+    Friend WithEvents btnTahoma As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents btnLoadpicture As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Objectdemo
+
+    ' Form Load event
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("Form has loaded successfully!")  ' Changed MsgBox to MessageBox.Show for consistency
+    End Sub
+
+    ' Button1 Click event
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Button Click")  ' Using MessageBox.Show for consistency
+    End Sub
+
+    ' Button3 Click event
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show("Button Click")  ' Changed MsgBox to MessageBox.Show
+    End Sub
+
+    ' btnLoadpicture Click event
+    Private Sub btnLoadpicture_Click(sender As Object, e As EventArgs) Handles btnLoadpicture.Click
+        MessageBox.Show("Picture load button was clicked!")  ' Changed MsgBox to MessageBox.Show
+    End Sub
+
+    ' btnGaramond Click event
+    Private Sub btnGaramond_Click(sender As Object, e As EventArgs) Handles btnGaramond.Click
+        MessageBox.Show("Garamond font has been selected!")  ' Changed MsgBox to MessageBox.Show
+    End Sub
+
+    ' btnMagneto Click event
+    Private Sub btnMagneto_Click(sender As Object, e As EventArgs) Handles btnMagneto.Click
+        MessageBox.Show("Magneto font has been selected!")  ' Changed MsgBox to MessageBox.Show
+    End Sub
+
+    ' btnTahoma Click event
+    Private Sub btnTahoma_Click(sender As Object, e As EventArgs) Handles btnTahoma.Click
+        MessageBox.Show("Tahoma font has been selected!")  ' Changed MsgBox to MessageBox.Show
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to exit?", "Exit Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If result = DialogResult.Yes Then
+            ' Close the form if the user clicks Yes
+            Me.Close()
+        Else
+            ' Do nothing if the user clicks No
+            ' This will simply cancel the closing action
+        End If
+    End Sub
+End Class
